d63ef0f41c0b32d7c361861c53549a8d285e07b5
Yuri Loureiro <loureirz@gmail.com>
2025-02-09 15:57:34 +0000

    fix token JSON

--- frontend/src/utils/token.js
+++ frontend/src/utils/token.js
@@ -1,30 +1,24 @@
+// token.js
 const tokenKey = "user-token";
 
-// Função para armazenar o token no localStorage
 export const setToken = (token) => {
     if (token) {
-        // Armazena o token como string no formato JSON
-        localStorage.setItem(tokenKey, JSON.stringify({ token }));
+        localStorage.setItem(tokenKey, token);
         console.log("Token armazenado:", token);
     } else {
         console.error("Tentativa de armazenar token vazio.");
     }
 };
 
-// Função para recuperar o token do localStorage
 export const getToken = () => {
-    const storedData = localStorage.getItem(tokenKey);
-    console.log("Token recuperado do storage:", storedData);
-
-    if (!storedData) {
+    const token = localStorage.getItem(tokenKey);
+    console.log("Token recuperado do storage:", token);
+    if (!token) {
         throw new Error("Token não encontrado ou está vazio.");
     }
-
-    const parsedData = JSON.parse(storedData); // Fazendo parse do objeto JSON
-    return parsedData.token; // Retorna apenas a propriedade "token"
+    return token;
 };
 
-// Função para remover o token do localStorage
 export const removeToken = () => {
     localStorage.removeItem(tokenKey);
     console.log("Token removido.");
