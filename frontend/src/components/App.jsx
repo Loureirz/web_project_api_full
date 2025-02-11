66328c2b5571b1c2f1c2b316e19827cef78798f2
Yuri Loureiro <loureirz@gmail.com>
2025-02-11 16:45:50 +0000

    fix handlecardlike

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -110,29 +110,18 @@ function App() {
   }
 
   const handleCardLike = (card) => {
-    const isLiked = card.likes.some((item) => item === currentUser.data._id);
-    console.log(currentUser.data._id);
+    const isLiked = card.likes.some((like) => like._id === currentUser.data._id);
   
     api
       .changeLikeCardStatus(card._id, isLiked)
-      .then((newCard) => {
-  
-        // Atualiza o estado de cards, substituindo apenas o card que mudou
-        setCards((prevCards) => {
-          if (prevCards.length === 0) {
-            return prevCards; // Retorna o estado anterior sem mudanças
-          }
-          
-          // Retorna uma nova lista de cards, substituindo apenas o card atualizado
-          const updatedCards = prevCards.map((c) => 
-            c._id === card._id ? newCard : c
-          );
-
-          return updatedCards;
-        });
+      .then((updatedCard) => {
+        setCards((prevCards) =>
+          prevCards.map((c) => (c._id === card._id ? updatedCard : c))
+        );
       })
       .catch((error) => console.log("Erro ao atualizar o like:", error));
   };
+  
   
 
   function handleDeleteClick(card) {
