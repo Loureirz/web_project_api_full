547c1d5e4163f2fdba814504a015cbfb88246628
Yuri Loureiro <loureirz@gmail.com>
2025-02-11 17:31:41 +0000

    fix card

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -6,7 +6,7 @@ function Card({ card, onCardClick, onCardLike, onCardDelete }) {
     const { currentUser } = useContext(CurrentUserContext);
     const { link, name, owner, likes } = card;
 
-    const isOwn = owner?._id === currentUser.data._id;
+    const isOwn = owner?._id === currentUser._id;
     const isLiked = card.likes.some((like) => like._id === currentUser._id);
 
     const cardLikeButtonClassName = `elements__like-button ${isLiked ? "active" : ""}`;
