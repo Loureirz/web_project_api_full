e71b7230e284a57d59e33d13dde24fba3373d94b
Yuri Loureiro <loureirz@gmail.com>
2025-02-12 16:28:02 +0000

    teste4

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -7,7 +7,7 @@ function Card({ card, onCardClick, onCardLike, onCardDelete }) {
     const { link, name, owner, likes } = card;
 
     const isOwn = (typeof owner === "object" ? owner._id : owner) === currentUser.data._id;
-    const isLiked = card.likes.some((like) => like._id === currentUser.data._id);
+    const isLiked = card.likes.some((like) => like.data._id === currentUser.data._id);
 
 
     const cardLikeButtonClassName = `elements__like-button ${isLiked ? "active" : ""}`;
