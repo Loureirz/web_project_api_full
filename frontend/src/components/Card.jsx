3b17240ec3f55139fef1b244f5960094b44f408f
Yuri Loureiro <loureirz@gmail.com>
2025-02-12 12:34:07 +0000

    teste3

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -4,7 +4,7 @@ function Card({ card, onCardClick, onCardLike, onCardDelete, userId }) {
     const { link, name, owner, likes } = card;
 
     const isOwn = card.owner === userId;
-    const isLiked = card.likes.some((like) => like._id === userId.data);
+    const isLiked = card.likes.some((like) => like._id === userId.data._id);
 
     const cardLikeButtonClassName = `elements__like-button ${isLiked ? "active" : ""}`;
     const cardDeleteButtonClassName = `elements__delete-button ${isOwn ? "elements__delete-button-hidden" : ""}`;
