8557826f44fe029f78b10643277221c57644dcfb
Yuri Loureiro <loureirz@gmail.com>
2025-02-13 10:22:03 +0000

    cleaning token.js

--- frontend/src/utils/token.js
+++ frontend/src/utils/token.js
@@ -2,22 +2,13 @@
 const tokenKey = "user-token";
 
 export const setToken = (token) => {
-    if (token) {
-        localStorage.setItem(tokenKey, token);
-    } else {
-        console.error("Tentativa de armazenar token vazio.");
-    }
+    return localStorage.setItem(tokenKey, token);
 };
 
 export const getToken = () => {
-    const token = localStorage.getItem(tokenKey);
-    if (!token) {
-        throw new Error("Token não encontrado ou está vazio.");
-    }
-    return token;
+    return token = localStorage.getItem(tokenKey);
 };
 
 export const removeToken = () => {
-    localStorage.removeItem(tokenKey);
-    console.log("Token removido.");
+    return localStorage.removeItem(tokenKey);
 };
