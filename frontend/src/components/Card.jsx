b98b9f7d2c98f5a7e91193aea73e676dc2c408a0
Yuri Loureiro <loureirz@gmail.com>
2025-02-13 18:39:07 +0000

    pe

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -6,35 +6,25 @@ function Card({ card, onCardClick, onCardLike, onCardDelete }) {
     const { currentUser } = useContext(CurrentUserContext);
     const { link, name, owner, likes } = card;
 
-if (!currentUser || !currentUser.data) {
-  console.log("currentUser ainda não carregado!");
-  return null;
-}
+    if (!currentUser || !currentUser.data) {
+        console.log("currentUser ainda não carregado!");
+        return null;
+    }
 
-    const isOwn = card.owner?._id === currentUser?.data?._id;
     const isLiked = currentUser?.data?._id && card.likes.some((like) => like === currentUser.data._id);
 
-const cardLikeButtonClassName = `elements__like-button ${isLiked ? "active" : ""}`;
-const cardDeleteButtonClassName = `elements__delete-button ${isOwn ? "elements__delete-button-hidden" : ""}`;
+    const cardLikeButtonClassName = `elements__like-button ${isLiked ? "active" : ""}`;
 
-    function handleClick() {
-        onCardClick(card);
-    }
-
-    function handleDeleteClick() {
-        onCardDelete(card);
-    }
-
-    function handleCardLike() {
-        onCardLike(card);
-    }
-
+    // Lógica do isOwn diretamente dentro do JSX no botão de deletar
     return (
         <div className="elements__card">
-            <button type="button" className={cardDeleteButtonClassName} onClick={handleDeleteClick}>
+            <button 
+                type="button" 
+                className={`elements__delete-button ${owner?._id === currentUser?.data?._id ? "elements__delete-button-hidden" : ""}`} 
+                onClick={() => onCardDelete(card)}>
                 <img className="elements__delete-icon" src={trash} alt="delete button icon" />
             </button>
-            <img className="elements__card-image" src={link} alt="" onClick={handleClick} />
+            <img className="elements__card-image" src={link} alt="" onClick={() => onCardClick(card)} />
             <div className="elements__wrapper-text-and-like-button">
                 <p className="elements__card-name">{name}</p>
                 <div className="elements__like">
@@ -42,7 +32,7 @@ const cardDeleteButtonClassName = `elements__delete-button ${isOwn ? "elements__
                         id="likeButton" 
                         type="button" 
                         className={cardLikeButtonClassName}
-                        onClick={handleCardLike}>
+                        onClick={() => onCardLike(card)}>
                     </button>
                     <p className="elements__like-counter">{likes.length}</p>
                 </div>
@@ -52,3 +42,4 @@ const cardDeleteButtonClassName = `elements__delete-button ${isOwn ? "elements__
 }
 
 export default Card;
+
