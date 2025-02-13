6e6999fd1117fbda0a7406b75b119412e5d9a325
Yuri Loureiro <loureirz@gmail.com>
2025-02-13 11:40:41 +0000

    add console token

--- frontend/src/utils/token.js
+++ frontend/src/utils/token.js
@@ -2,13 +2,23 @@
 const tokenKey = "user-token";
 
 export const setToken = (token) => {
+    if (token) {
+        localStorage.setItem(tokenKey, token);
+    } else {
+        console.error("Tentativa de armazenar token vazio.");
+    }
     return localStorage.setItem(tokenKey, token);
 };
 
 export const getToken = () => {
-    return localStorage.getItem(tokenKey);
+    const token = localStorage.getItem(tokenKey);
+    if (!token) {
+        throw new Error("Token não encontrado ou está vazio.");
+    }
+    return token;
 };
 
 export const removeToken = () => {
-    return localStorage.removeItem(tokenKey);
+    localStorage.removeItem(tokenKey);
+    console.log("Token removido.");
 };
