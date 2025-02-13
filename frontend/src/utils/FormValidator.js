3447b23c6989a1ce62e79a97d897ff15c676dcc5
Yuri Loureiro <loureirz@gmail.com>
2025-02-13 10:19:45 +0000

    cleaning formvalidator

--- frontend/src/utils/FormValidator.js
+++ frontend/src/utils/FormValidator.js
@@ -10,25 +10,22 @@ export default class FormValidator {
     }
   }
 
-  // Exibe a mensagem de erro
   _showInputError(input, errorMessage) {
     const errorElement = this._formElement.querySelector(`#${input.id}-error`);
-    if (!errorElement) return; // Se o elemento de erro não existir, não faz nada
+    if (!errorElement) return;
     input.classList.add(this._config.inputErrorClass);
     errorElement.textContent = errorMessage;
     errorElement.classList.add(this._config.errorClassVisible);
   }
 
-  // Oculta a mensagem de erro
   _hideInputError(input) {
     const errorElement = this._formElement.querySelector(`#${input.id}-error`);
-    if (!errorElement) return; // Se o elemento de erro não existir, não faz nada
+    if (!errorElement) return;
     input.classList.remove(this._config.inputErrorClass);
     errorElement.classList.remove(this._config.errorClassVisible);
     errorElement.textContent = '';
   }
 
-  // Verifica a validade do input e exibe a mensagem de erro
   _checkInputValidity(input) {
     const errorElement = this._formElement.querySelector(`#${input.id}-error`);
     if (!input.validity.valid) {
@@ -37,7 +34,6 @@ export default class FormValidator {
       this._hideInputError(input);
     }
 
-    // Checa se o comprimento é válido
     if (input.value.trim().length === 0 || input.value.trim().length < 2) {
       this._checkInputLength(input, errorElement);
     } else {
@@ -45,7 +41,6 @@ export default class FormValidator {
     }
   }
 
-  // Verifica o comprimento do input
   _checkInputLength(input, errorElement) {
     if (!errorElement) return;
 
@@ -61,12 +56,10 @@ export default class FormValidator {
     }
   }
 
-  // Verifica se algum input é inválido
   _hasInvalidInput() {
     return this._inputList.some((input) => !input.validity.valid);
   }
 
-  // Atualiza o estado do botão de submit
   _toggleButtonState() {
     if (!this._submitButton) return;
 
@@ -79,7 +72,6 @@ export default class FormValidator {
     }
   }
 
-  // Define ouvintes de eventos para os inputs
   _setEventListeners() {
     this._inputList.forEach((input) => {
       input.addEventListener('input', () => {
@@ -89,14 +81,12 @@ export default class FormValidator {
     });
   }
 
-  // Ativa a validação
   enableValidation() {
     this._formElement.addEventListener('submit', (evt) => evt.preventDefault());
     this._setEventListeners();
-    this._toggleButtonState();  // Estado inicial do botão
+    this._toggleButtonState();
   }
 
-  // Reseta a validação (limpa erros e reabilita o botão)
   resetValidation() {
     this._inputList.forEach((input) => this._hideInputError(input));
     this._toggleButtonState();
