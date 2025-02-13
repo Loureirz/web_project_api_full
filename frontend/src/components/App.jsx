d8f1748939ee01a2367633b8949d5d31a68b7868
Yuri Loureiro <loureirz@gmail.com>
2025-02-13 12:49:18 +0000

    teste3

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -124,7 +124,7 @@ function App() {
       await api.addCard({ name: data.name, link: data.link, owner: data.owner });
       
       const updatedCards = await api.getInitialCards();
-      const updatedNew = updatedCards.sort((a, b) => new Date(a.created_at) - new Date(b.created_at));
+      const updatedNew = updatedCards.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
       setCards(updatedNew);
       
       closeAllPopups();
@@ -162,7 +162,7 @@ function App() {
               setCurrentUser(data);
               navigate("/");
               api.getInitialCards().then(data=>{
-                const updatedNew = data.sort((a, b) => new Date(a.created_at) - new Date(b.created_at));
+                const updatedNew = data.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
                 setCards(updatedNew);
              })
         }).catch(err => {
